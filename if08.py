07ca63fb61ad0be38675a50b0294bfad4e1f9825
ozodbektoshpulatov <toshpulatovozodbek96@gmail.com>
2023-07-09 22:11:18 +0500

    couldn't accomplish

--- if08.py
+++ if08.py
@@ -11,4 +11,17 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    a=int(input())
+    if 10<=a<=99:
+        if a%2==1:
+            return "two-digit odd number"
+        else: 
+            return "two-digit even number"
+    elif 100<=a<=999:
+        if a%2==1:
+            return "three-digit odd number"
+        else:
+            return "three-digit even number"
+    
+    return a
+print(main(28))
